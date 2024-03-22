ab43c8c9805b9c323a0091f428af7de991489340
Girthstorm <ayounathan05@gmail.com>
2024-03-22 16:21:36 -0700

    Final?

--- src/Components/BottomCardComponent.tsx
+++ src/Components/BottomCardComponent.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const BottomCardComponent = (prop: {card:string, text:string, company:string, posNeg:string, type:string, logo:string, headText:string, followers:string, statsColor:string, statLogo:string, stats:string}) => {
+const BottomCardComponent = (prop: { company:string, posNeg:string, type:string, logo:string, headText:string, followers:string, statsColor:string, statLogo:string, stats:string}) => {
   return (
     <div className=''>
-      <div className={`${prop.card} grid grid-cols-2 px-6 py-4 rounded-[5px]`}>
-        <div className={`${prop.text} boldInt whitespace-nowrap`}>{prop.type}</div>
+      <div className={'cursor-pointer bg-[#f0f2fa] dark:bg-[#252a41] dark:hover:bg-[#333A56] hover:bg-[#E1E3F0] grid grid-cols-2 px-6 py-4 rounded-[5px]'}>
+        <div className={'font-bold dark:text-[#8b97c6]  boldInt whitespace-nowrap'}>{prop.type}</div>
         <div className='grid justify-end'><img src={prop.logo} alt={`${prop.company} logo`}/></div>
-        <div className={`${prop.headText} font-bold text-[35px] leading-none mt-12`}>{prop.followers}</div>
+        <div className={`${prop.headText} dark:text-white font-bold text-[35px] leading-none mt-12`}>{prop.followers}</div>
         <div className={`text-end mt-auto leading-none ${prop.statsColor}`}><img className='inline me-2' src={prop.statLogo} alt={`${prop.posNeg} arrow`}/>{prop.stats}</div>
       </div>
     </div>
--- src/Components/TopCardComponent.tsx
+++ src/Components/TopCardComponent.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../App.css';
 //This was heavily inspired by Brandon Nguyen, I changed styling by moving it from style tags to classes, added a prop to give the alt tag the corresponding company alt tag and positive or negative
-const TopCardComponent = (prop: {card:string, company:string, posNeg:string, topBorder:string, logo:string, username:string, headText:string, followers:string, type:string, statsColor:string, statLogo:string, stats:string}) => {
+const TopCardComponent = (prop: { company:string, posNeg:string, topBorder:string, logo:string, username:string, followers:string, type:string, statsColor:string, statLogo:string, stats:string}) => {
   return (
     <div>
-      <div className={`${prop.card} text-center rounded-[5px] bg-[#f0f2fa] dark:bg-[#252a41]`}>
+      <div className={` cursor-pointer dark:hover:bg-[#333A56] hover:bg-[#E1E3F0] text-center rounded-[5px] bg-[#f0f2fa] dark:bg-[#252a41]`}>
         <div className={`h-[6px] rounded-t-[5px] ${prop.topBorder}`}></div>
-        <p className="font-bold dark:text-[#8b97c6;]"><img className='inline me-2 my-6 h-5' src={prop.logo} alt={`${prop.company} logo`}/>{prop.username}</p>
-        <h1 className={`${prop.headText} font-bold  text-[50px] dark:text-white leading-none`}>{prop.followers}</h1>
+        <p className="font-bold dark:text-[#8b97c6]"><img className='inline me-2 my-6 h-5' src={prop.logo} alt={`${prop.company} logo`}/>{prop.username}</p>
+        <h1 className={` font-bold  text-[50px] dark:text-white leading-none`}>{prop.followers}</h1>
         <p className={`tracking-[0.25em] dark:text-[#8b97c6;] font-bold`}>{prop.type}</p>
         <p className={`${prop.statsColor} bold`}><img className='inline me-2 my-8' src={prop.statLogo} alt={`${prop.posNeg} arrow`}/>{prop.stats}</p>
       </div>
